11d127138ee7f9f665a5d1516f91cbd7449d725d
mehul1110 <mehulshra1110@gmail.com>
2026-02-03 15:24:00 +0530

    UC1: Create Vessel Model class with attributes, constructors, and getters/setters

---
+++ src/Vessel.java
@@ -0,0 +1,65 @@
+/**
+ * Vessel Model Class
+ * Represents a cargo vessel with its performance attributes.
+ */
+public class Vessel {
+    private String vesselId;
+    private String vesselName;
+    private double averageSpeed;
+    private String vesselType;
+
+    /**
+     * No-argument constructor
+     */
+    public Vessel() {
+    }
+
+    /**
+     * Parameterized constructor (four-argument constructor)
+     * 
+     * @param vesselId Unique ID of the vessel
+     * @param vesselName Name of the vessel
+     * @param averageSpeed Average speed in knots
+     * @param vesselType Type of the vessel (e.g., Cargo, Tanker, Cruise)
+     */
+    public Vessel(String vesselId, String vesselName, double averageSpeed, String vesselType) {
+        this.vesselId = vesselId;
+        this.vesselName = vesselName;
+        this.averageSpeed = averageSpeed;
+        this.vesselType = vesselType;
+    }
+
+    // Getters and Setters
+
+    public String getVesselId() {
+        return vesselId;
+    }
+
+    public void setVesselId(String vesselId) {
+        this.vesselId = vesselId;
+    }
+
+    public String getVesselName() {
+        return vesselName;
+    }
+
+    public void setVesselName(String vesselName) {
+        this.vesselName = vesselName;
+    }
+
+    public double getAverageSpeed() {
+        return averageSpeed;
+    }
+
+    public void setAverageSpeed(double averageSpeed) {
+        this.averageSpeed = averageSpeed;
+    }
+
+    public String getVesselType() {
+        return vesselType;
+    }
+
+    public void setVesselType(String vesselType) {
+        this.vesselType = vesselType;
+    }
+}
